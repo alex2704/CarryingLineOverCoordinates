87ac492ff20402991297771cc89835ee36824377
alex2704 <34662549+alex2704@users.noreply.github.com>
2018-10-09 16:30:39 +0300

    first commit

---
+++ Task3/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel = new System.Windows.Forms.Panel();
+            this.Transfer_btn = new System.Windows.Forms.Button();
+            this.draw_myCurve_btn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Clear_btn = new System.Windows.Forms.Button();
+            this.panel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.Clear_btn);
+            this.panel.Controls.Add(this.Transfer_btn);
+            this.panel.Controls.Add(this.draw_myCurve_btn);
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(966, 100);
+            this.panel.TabIndex = 0;
+            // 
+            // Transfer_btn
+            // 
+            this.Transfer_btn.Location = new System.Drawing.Point(287, 20);
+            this.Transfer_btn.Name = "Transfer_btn";
+            this.Transfer_btn.Size = new System.Drawing.Size(230, 61);
+            this.Transfer_btn.TabIndex = 1;
+            this.Transfer_btn.Text = "перенести прямую";
+            this.Transfer_btn.UseVisualStyleBackColor = true;
+            this.Transfer_btn.Click += new System.EventHandler(this.Transfer_btn_Click);
+            // 
+            // draw_myCurve_btn
+            // 
+            this.draw_myCurve_btn.Location = new System.Drawing.Point(20, 20);
+            this.draw_myCurve_btn.Name = "draw_myCurve_btn";
+            this.draw_myCurve_btn.Size = new System.Drawing.Size(215, 61);
+            this.draw_myCurve_btn.TabIndex = 0;
+            this.draw_myCurve_btn.Text = "нарисовать кривую";
+            this.draw_myCurve_btn.UseVisualStyleBackColor = true;
+            this.draw_myCurve_btn.Click += new System.EventHandler(this.draw_myCurve_btn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Clear_btn
+            // 
+            this.Clear_btn.Location = new System.Drawing.Point(585, 20);
+            this.Clear_btn.Name = "Clear_btn";
+            this.Clear_btn.Size = new System.Drawing.Size(187, 61);
+            this.Clear_btn.TabIndex = 2;
+            this.Clear_btn.Text = "Очистить";
+            this.Clear_btn.UseVisualStyleBackColor = true;
+            this.Clear_btn.Click += new System.EventHandler(this.Clear_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(990, 561);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button draw_myCurve_btn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Transfer_btn;
+        private System.Windows.Forms.Button Clear_btn;
+    }
+}
+
---
+++ Task3/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        const int n = 4;
+        Graphics g;
+        Graphics g1;
+        SolidBrush mySolidbrush;
+        Pen myPen;
+        Bitmap bitmap;
+        int count = n;
+        PointF[] p = new PointF[n];
+        PointF[] a = new PointF[n];
+        int check = 1;
+        bool drawing = false;
+        public Form1()
+        {
+            InitializeComponent();
+            draw_myCurve_btn.Enabled = false;
+            myPen = new Pen(Color.Black,1);
+            bitmap = new Bitmap(ClientSize.Width,
+              ClientSize.Height);
+            g = Graphics.FromImage(bitmap);
+            g1 = this.CreateGraphics();
+            Transfer_btn.Enabled = false;
+        }
+        private void DrawArr(PointF[] p)
+        {
+            myPen = new Pen(Color.Black,2);
+            mySolidbrush = new SolidBrush(Color.Red);
+            for (int i = 0; i < n; i++)
+                g.FillRectangle(mySolidbrush, p[i].X,p[i].Y,6,6);
+        }
+        PointF[] StepPoint = new PointF[n];
+        private static void CheckDistance(PointF[] p, PointF[] a, PointF[] StepPoint)
+        {
+            float distanceX, distanceY;
+            float stepX, stepY;
+            for (int i = 0; i < p.Length; i++)
+            {
+                distanceX = a[i].X - p[i].X;
+                stepX = distanceX / 100;
+                distanceY = a[i].Y - p[i].Y;
+                stepY = distanceY / 100;
+                StepPoint[i] = new PointF(stepX, stepY);
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (count > 0 && check % 2 != 0)
+            {
+                p[n - count] = new Point(e.X,e.Y);
+                g.DrawRectangle(myPen, e.X, e.Y, 2,2);
+                count--;
+            }
+            else if (count > 0 && check % 2 == 0)
+            {
+                a[n - count] = new Point(e.X, e.Y);
+                g.DrawRectangle(myPen, e.X, e.Y, 2, 2);
+                count--;
+            }
+            if(count == 0 && check % 2 != 0)
+            {
+                draw_myCurve_btn.Enabled = true;
+                drawing = true;
+                DrawArr(p);
+            }
+            else if(count == 0 && check % 2 == 0)
+            {
+                Transfer_btn.Enabled = true;
+                DrawArr(a);
+            }
+            g1.DrawImage(bitmap, ClientRectangle);
+        }
+
+        private void draw_myCurve_btn_Click(object sender, EventArgs e)
+        {
+            if (drawing && check % 2 != 0)
+                g.DrawCurve(myPen, p, 1);
+            g1.DrawImage(bitmap, ClientRectangle);
+            drawing = false;
+            count = 4;
+            check++;
+            draw_myCurve_btn.Enabled = false;
+        }
+        Pen specialPen = new Pen(Color.White,2);
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                g.DrawCurve(specialPen, p);
+                p[0].X += StepPoint[0].X;
+                p[0].Y += StepPoint[0].Y;
+                p[1].X += StepPoint[1].X;
+                p[1].Y += StepPoint[1].Y;
+                p[2].X += StepPoint[2].X;
+                p[2].Y += StepPoint[2].Y;
+                p[3].X += StepPoint[3].X;
+                p[3].Y += StepPoint[3].Y;
+                g.DrawCurve(myPen,p);
+                g1.DrawImage(bitmap, ClientRectangle);
+            if (p[0].X - 0.01 <= a[0].X && p[0].X + 0.01 >= a[0].X)
+            {
+                timer1.Enabled = false;
+                Transfer_btn.Enabled = false;
+                draw_myCurve_btn.Enabled = false;
+            }
+        }
+
+        private void Transfer_btn_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+
+            }
+            else
+            {
+                timer1.Enabled = true;
+            }
+            CheckDistance(p, a, StepPoint);
+        }
+
+        private void Clear_btn_Click(object sender, EventArgs e)
+        {
+            Color cl = Color.FromArgb(255, 255, 255);
+            g1.Clear(cl);
+            g.Clear(cl);
+            drawing = false;
+            count = n;
+            check = 1;
+        }
+    }
+}
